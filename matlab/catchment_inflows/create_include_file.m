f4828477f6b6aeba2ebde0d95e8f1ecb795742f2
brendan-busch <brendan.busch@uwa.edu.au>
2020-04-20 14:20:51 +0800

    Fix to include_catchment

--- matlab/catchment_inflows/create_include_file.m
+++ matlab/catchment_inflows/create_include_file.m
@@ -7,10 +7,11 @@ fid = fopen('include_catchment.fvc','wt');
 
 outdir = 'Flow_Catch\';
 
+tidename = '..\BCs\Tide_v2\Fremantle_Inflow_2019_ARMS_filtered.csv';
 
 fprintf(fid,'! Fremantle___________________________________________________________\n');
 
-fprintf(fid,'bc == WL,1, ..\BCs\Tide_v2\Fremantle_Inflow_2019_ARMS_filtered.csv\n');
+fprintf(fid,'bc == WL,1,%s\n',tidename);
   fprintf(fid,'bc header ==  ISOTime,wl,Sal,Temp,zeroes,zeroes,TSS,TSS,Oxy,Sil,Amm,Nit,FRP,FRP,DOC_T,POC_T,DON_T,PON_T,OP,OP,DOC_T,DON_T,OP,POC_T,CHLA,CHLA,CHLA,CHLA,CHLA,CHLA\n');
   fprintf(fid,'bc scale == 1,1,1,1,1,0.3,0.7,1,1,1,1,1,0.1,0.1,0.5,0.1,1,0.3,0.5,0.9,0.1,0.2,0.5,0.167,0.125,0.333,2.292,1.25,0.00754717\n');
 fprintf(fid,'end bc');
