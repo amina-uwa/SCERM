8598146eaa9d5c195245b3c296fb73eacd8adee2
brendan-busch <brendan.busch@uwa.edu.au>
2020-04-20 13:49:34 +0800

    Catchment BC files

---
+++ matlab/catchment_inflows/build_catchment_inflows.m
@@ -0,0 +1,167 @@
+clear all; close all;
+
+addpath(genpath('../Inflows'));
+
+shp = shaperead('../../../Source_to_SCERM/Maps/InflowPoints.shp');
+
+inflowfile = dir(['../Inflows/BCs/Flow/','*.csv']);
+
+load('data.mat');
+
+outdir = 'Flow_Catch/';
+
+if ~exist(outdir,'dir')
+    mkdir(outdir);
+end
+
+for i = 1:length(inflowfile)
+    
+    fname = regexprep(inflowfile(i).name,'.csv','');
+    
+    inflow.(fname) = tfv_readBCfile(['../Inflows/BCs/Flow/',inflowfile(i).name]);
+    
+end
+
+thetime = datenum(2017,01,01):01:datenum(2019,01,01);
+
+
+inflownames = {...
+    'Bayswater_Inflow',...
+    'Bennett_Inflow',...
+    'Canning_Inflow',...
+    'Ellenbrook_Inflow',...
+    'Helena_Inflow',...
+    'Jane_Inflow',...
+    'Susannah_Inflow',...
+    'Upper_Swan_Inflow_2000',...
+    };
+
+
+
+
+catname = {...
+    'Bayswater',...
+    'Bennett Brook',...
+    'Upper Canning',...
+    'Ellen Brook',...
+    'Helena River',...
+    'Jane Brook',...
+    'Susannah',...
+    'Millendon',...
+    };
+
+vars = fieldnames(inflow.Bayswater_Inflow);
+
+
+catsites = fieldnames(data);
+
+% for i = 1:length(catsites)
+%
+%     cs = catsites{i};
+%
+%     isfound = 0;
+%
+%     for j = 1:length(shp)
+%         ss = shp(j).nNames;
+%
+%         if strcmpi(ss,cs) == 1
+%             isfound = 1;
+%         end
+%     end
+%     if ~isfound
+%         stop
+%     end
+% end
+%
+
+for i = 1:length(shp)
+    
+    sss = find(strcmpi(catname,shp(i).Nearest8) == 1);
+    
+    
+    thesite = shp(i).nNames;
+    theBC = inflownames{sss};
+    
+    
+    outfile = [outdir,shp(i).SubCat,'.csv'];
+    fid = fopen(outfile,'wt');
+    
+    for j = 1:length(vars)
+        if j == length(vars)
+            fprintf(fid,'%s\n',vars{j});
+        else
+            fprintf(fid,'%s,',vars{j});
+        end
+    end
+    
+    % Now the write loop
+    
+    for k = 1:length(thetime)
+        
+        index_bc = find(inflow.(theBC).Date == thetime(k));
+        index_cat = find(data.(thesite).Flow.Date == thetime(k));
+        
+        TN = inflow.(theBC).Amm(index_bc) + ...
+            inflow.(theBC).Nit(index_bc) + ...
+            inflow.(theBC).DON_T(index_bc) + ...
+            inflow.(theBC).PON_T(index_bc);
+        
+        TP = inflow.(theBC).FRP(index_bc) + ...
+            inflow.(theBC).OP(index_bc);
+        
+        
+        fprintf(fid,'%s,',datestr(thetime(k),'dd/mm/yyyy HH:MM:SS'));
+        
+        for j = 2:length(vars)
+            
+            pvar = 0;
+            
+            switch vars{j}
+                case 'Flow'
+                    pvar = data.(thesite).Flow.Data(index_cat);
+                case 'Amm'
+                    theconv = inflow.(theBC).Amm(index_bc) / TN;
+                    pvar = data.(thesite).TN.Data(index_cat) * theconv;
+                case 'Nit'
+                    theconv = inflow.(theBC).Nit(index_bc) / TN;
+                    pvar = data.(thesite).TN.Data(index_cat) * theconv; 
+                case 'DON_T'
+                    theconv = inflow.(theBC).DON_T(index_bc) / TN;
+                    pvar = data.(thesite).TN.Data(index_cat) * theconv;
+                case 'PON_T'
+                    theconv = inflow.(theBC).DON_T(index_bc) + ...
+                        inflow.(theBC).Nit(index_bc) + ...
+                        inflow.(theBC).Amm(index_bc);
+                    pvar = data.(thesite).TN.Data(index_cat) - theconv;                    
+                    
+                case 'FRP'
+                    theconv = inflow.(theBC).FRP(index_bc) / TP;
+                    pvar = data.(thesite).TP.Data(index_cat) * theconv;
+                case 'OP'
+                    theconv = inflow.(theBC).FRP(index_bc) * 2;
+                    pvar = data.(thesite).TP.Data(index_cat) - theconv;
+                otherwise
+                    
+                    pvar =inflow.(theBC).(vars{j})(index_bc);
+            end
+            
+            pvar(pvar < 0) = 0;
+            
+            if j == length(vars)
+                fprintf(fid,'%4.4f\n',pvar);
+            else
+                fprintf(fid,'%4.4f,',pvar);
+            end
+        end
+    end
+    
+    fclose(fid);
+end
+                    
+                    
+plot_bcfiles(outdir)                   
+            
+            
+            
+            
+            
---
+++ matlab/catchment_inflows/import_catchment_data.m
@@ -0,0 +1,82 @@
+clear all; close all;
+
+addpath(genpath('tuflowfv'));
+
+fid = fopen('..\..\..\Source_to_SCERM\SourceOutputs\40 - Scenario 1 - 2020-03-17T11-09-58.res.csv','rt');
+
+
+isdata = 0;
+linecount = 1;
+
+while isdata == 0
+    fline = fgetl(fid);
+    
+    sLine = strsplit(fline,',');
+    
+    if strcmpi(sLine{1},'Date') == 1
+        isdata = 1;
+    else
+        
+     linecount = linecount + 1;   
+    end 
+end
+  
+for i = 2:length(sLine)
+    
+    tt = strsplit(sLine{i},'>');
+    header(i-1) = {[tt{2},'_',tt{3}]};
+    header(i-1) = regexprep(header(i-1),' ','_');
+    header(i-1) = regexprep(header(i-1),'@','_');
+    header(i-1) = regexprep(header(i-1),'-','_');
+    header(i-1) = regexprep(header(i-1),'_Outflow_Constituents','');
+    header(i-1) = regexprep(header(i-1),'_Upstream_Flow_Concentration','');
+    header(i-1) = regexprep(header(i-1),'Outflow_Upstream_','');
+    header(i-1) = regexprep(header(i-1),'___','_');
+end
+    
+
+
+
+frewind(fid)
+x  = length(sLine);
+textformat = [repmat('%s ',1,x)];
+% read single line: number of x-values
+datacell = textscan(fid,textformat,'Headerlines',linecount + 1,'Delimiter',',');
+fclose(fid);
+
+
+mdate = datenum(datacell{1},'yyyy-mm-dd');
+
+for i = 1:length(header)
+    
+    ttt = regexprep(header{i},'_Flow','.Flow');
+    ttt = regexprep(ttt,'_TN','.TN');
+    ttt = regexprep(ttt,'_TP','.TP');
+    sss = strsplit(ttt,'.');
+    
+    data.(sss{1}).(sss{2}).Date =  mdate;
+    data.(sss{1}).(sss{2}).Data =str2double(datacell{i+1});
+    
+    switch sss{2}
+        case 'TN' % in kg/m3
+            data.(sss{1}).(sss{2}).Data = data.(sss{1}).(sss{2}).Data * ((1000*1000)/14); % to mmol/m3
+        case 'TP'
+            data.(sss{1}).(sss{2}).Data = data.(sss{1}).(sss{2}).Data * ((1000*1000)/31);% to mmol/m3
+        otherwise
+            
+    end
+end
+
+
+save data.mat data -mat;
+
+sites = fieldnames(data);
+
+fid = fopen('Sites.csv','wt');
+
+for i = 1:length(sites)
+    fprintf(fid,'%s\n',sites{i});
+end
+fclose(fid);
+
+
