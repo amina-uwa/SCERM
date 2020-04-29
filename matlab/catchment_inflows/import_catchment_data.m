e1645b9ae313f32d44502622b3c52bb1167d32df
brendan-busch <brendan.busch@uwa.edu.au>
2020-04-29 16:32:52 +0800

    Updated Catchment BC files and 2010 fvc file

--- matlab/catchment_inflows/import_catchment_data.m
+++ matlab/catchment_inflows/import_catchment_data.m
@@ -2,7 +2,7 @@ clear all; close all;
 
 addpath(genpath('tuflowfv'));
 
-fid = fopen('..\..\..\Source_to_SCERM\SourceOutputs\GoodNutrientsBadFlow_3 - Scenario 1 - 2020-04-24T18-52-36.res.csv','rt');
+fid = fopen('..\..\..\Source_to_SCERM\SourceOutputs\GoodNutrientsBadFlow_3 - Zones - 2020-04-29T15-54-35.res.csv','rt');
 
 
 isdata = 0;
