8c76d31089440203906537e9e00686e2bc7d95a2
brendan.busch@uwa.edu.au <brendan.busch@uwa.edu.au>
2020-06-18 11:27:03 +0800

    Update export_data_from_domain.m

--- matlab/prawn_export_v1/export_data_from_domain.m
+++ matlab/prawn_export_v1/export_data_from_domain.m
@@ -2,8 +2,8 @@
 
 addpath(genpath('tuflowfv'));
 
-ncdir = 'Q:\SCERM\SCERM\Output/';
-savedir = 'F:\Dropbox\SCERM_Proc/';
+ncdir = 'Q:\SCERM\SCERM\Output\ALL/';
+savedir = 'F:\Dropbox\SCERM_Proc_More/';
 
 dirlist = dir([ncdir,'*.nc']);
 
@@ -31,15 +31,30 @@ dirlist = dir([ncdir,'*.nc']);
 %     'WQ_DIAG_PHY_MPB',...
 %     'WQ_DIAG_PHY_BPP',...
 %     };
+% vars = {...
+%     'SAL',...
+%     'TEMP',...
+%     'WQ_OXY_OXY',...
+%     };
+
 vars = {...
     'SAL',...
+    'TEMP',...
+    'WQ_OXY_OXY',...
+    'V_x',...
+    'V_y',...
+    'WQ_DIAG_TOT_TSS',...
+    'WQ_DIAG_TOT_TURBIDITY',...
+    'WQ_DIAG_TOT_LIGHT',...
+    'WQ_DIAG_TOT_PAR',...
+    'WQ_DIAG_OXY_SAT',...
     };
 
 
-%stop;
 
 
-for bdb = 1:length(dirlist)
+
+for bdb = 9:length(dirlist)%1:length(dirlist)
     
     ncfile = [ncdir,dirlist(bdb).name];
     %ncfile = 'Z:\Busch\Studysites\Peel\2018_Modelling\Peel_WQ_Model_v5_2016_2017_3D_Murray\Output\sim_2016_2017_Open.nc';
@@ -50,72 +65,89 @@ for bdb = 1:length(dirlist)
     
     the_vars = tfv_infonetcdf(ncfile);
     
+    
+    
     disp(ncfile);
     
-    if ~exist(outdir,'dir')
+    %if ~exist(outdir,'dir')
+    
+    mkdir(outdir);
+    %outdir = 'I:\Peel\Matfiles/Peel_WQ_Model_v5_2016_2017_3D_Murray/';
+    
+    %load Export_Locations.mat;
+    %shp = S;
+    
+    
+    mdata = tfv_readnetcdf(ncfile,'time',1);
+    Time = mdata.Time;
+    %sites = {'Currency Creek';'Lake Albert WLR';'Lake Alex Middle'};
+    
+    %______________________________________________________________
+    
+    
+%     surf_ind = data.idx3;
+%     
+%     bottom_ind(1:length(data.idx3)-1) = data.idx3(2:end) - 1;
+%     bottom_ind(length(data.idx3)) = length(data.idx3);
+    
+    
+    for i = 1:length(vars)
         
-        mkdir(outdir);
-        %outdir = 'I:\Peel\Matfiles/Peel_WQ_Model_v5_2016_2017_3D_Murray/';
         
-        %load Export_Locations.mat;
-        %shp = S;
+        if strcmpi(vars{i},'SAL') == 1
+            data = tfv_readnetcdf(ncfile,'names',{'idx2';'idx3';'cell_X';'cell_Y';'cell_A';'D';'cell_Zb';'layerface_Z';'NL';});
+        end
         
-        data = tfv_readnetcdf(ncfile,'names',{'idx2';'idx3';'cell_X';'cell_Y';'cell_A'});
-        
-        mdata = tfv_readnetcdf(ncfile,'time',1);
-        Time = mdata.Time;
-        %sites = {'Currency Creek';'Lake Albert WLR';'Lake Alex Middle'};
-        
-        %______________________________________________________________
+        disp(['Importing ',vars{i}]);
         
         
-        surf_ind = data.idx3;
-        
-        bottom_ind(1:length(data.idx3)-1) = data.idx3(2:end) - 1;
-        bottom_ind(length(data.idx3)) = length(data.idx3);
-        
-        
-        for i = 1:length(vars)
-            disp(['Importing ',vars{i}]);
+        if sum(strcmpi(the_vars,vars{i})) == 1 | ...
+                strcmpi(vars{i},'WQ_DIAG_PHY_TCHLA') == 1
             
-            
-            if sum(strcmpi(the_vars,vars{i})) == 1 | ...
-                    strcmpi(vars{i},'WQ_DIAG_PHY_TCHLA') == 1
+            if strcmpi(vars{i},'WQ_DIAG_PHY_TCHLA') == 0
+                mod = tfv_readnetcdf(ncfile,'names',vars(i));
                 
-                if strcmpi(vars{i},'WQ_DIAG_PHY_TCHLA') == 0
-                    mod = tfv_readnetcdf(ncfile,'names',vars(i));
-                    
-                    
+                if strcmpi(vars{i},'SAL') == 1
                     savedata.X = data.cell_X;
                     savedata.Y = data.cell_Y;
                     savedata.Area = data.cell_A;
                     savedata.Time = Time;
+                    savedata.(vars{i}) = mod.(vars{i});
+                    savedata.D = data.D;
+                    savedata.Zb = data.cell_Zb;
+                    savedata.layerface_Z = data.layerface_Z;
+                    savedata.NL = data.NL;
+                    savedata.idx3 = data.idx3;
+                    savedata.idx2 = data.idx2;
+                else
+                    savedata.(vars{i}) = mod.(vars{i});
                 end
-                
-                switch vars{i}
-                    
-                    case 'H'
-                        savedata.(vars{i}) = mod.(vars{i});
-                    case 'D'
-                        savedata.(vars{i}) = mod.(vars{i});
-                    case 'cell_A'
-                        savedata.(vars{i}) = mod.(vars{i});
-                    case  'WQ_DIAG_PHY_TCHLA'
-                        savedata = run_tchla_cal(outdir);
-                    otherwise
-                        savedata.(vars{i}).Top = mod.(vars{i})(surf_ind,:);
-                        savedata.(vars{i}).Bot = mod.(vars{i})(bottom_ind,:);
-                        
-                end
-                
-                
-                
-                save([outdir,vars{i},'.mat'],'savedata','-mat','-v7.3');
-                clear savedata;
             end
             
+            %                 switch vars{i}
+            %
+            %                     case 'H'
+            %                         savedata.(vars{i}) = mod.(vars{i});
+            %                     case 'D'
+            %                         savedata.(vars{i}) = mod.(vars{i});
+            %                     case 'cell_A'
+            %                         savedata.(vars{i}) = mod.(vars{i});
+            %                     case  'WQ_DIAG_PHY_TCHLA'
+            %                         savedata = run_tchla_cal(outdir);
+            %                     otherwise
+            %                         savedata.(vars{i}).Top = mod.(vars{i})(surf_ind,:);
+            %                         savedata.(vars{i}).Bot = mod.(vars{i})(bottom_ind,:);
+            %
+            %                 end
+            
+            
+            
+            save([outdir,vars{i},'.mat'],'savedata','-mat','-v7.3');
+            clear savedata;
         end
+        
     end
+    %end
 end
 
 
