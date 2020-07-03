c171486a52863f23ae4d934ec48cb975ed522406
brendan-busch <brendan.busch@uwa.edu.au>
2020-07-03 11:52:17 +0800

    Changes to SCERM44 & SCERM44 Tracer include files for BC scaling

--- matlab/catchment_inflows/create_include_file.m
+++ matlab/catchment_inflows/create_include_file.m
@@ -32,8 +32,13 @@ Y = shp(i).Y;
 
 fprintf(fid,'bc == QC,%10.4f,%10.4f,%s\n',X,Y,filename);
   fprintf(fid,'bc header ==  ISOTime,Flow,Sal,Temp,zeroes,zeroes,TSS,TSS,Oxy,Sil,Amm,Nit,FRP,FRP,DOC_T,POC_T,DON_T,PON_T,OP,OP,DOC_T,DON_T,OP,POC_T,CHLA,CHLA,CHLA,CHLA,CHLA,CHLA\n');
-  fprintf(fid,'bc scale == 1,1,1,1,1,0.3,0.7,1,1,1,1,1,0.1,0.1,0.5,0.3,1,0.3,0.5,0.9,0.7,0.2,0.5,0.625,0.417,0.417,0.625,2.083,0.00754717\n');
-fprintf(fid,'end bc\n');
+  
+  if strcmpi(shp(i).SubCat,'Upper Swan01 Outflow') == 1
+    fprintf(fid,'bc scale == 1,1,1,1,1,0.3,0.7,1,1,1,1,1,0.1,0.1,0.5,0.3,1,0.3,0.5,0.9,0.7,0.2,0.5,0.625,0.417,0.417,0.625,2.083,0.00754717\n');
+  else
+    fprintf(fid,'bc scale == 0.95,1,1,1,1,0.3,0.7,1,1,1,1,1,0.1,0.1,0.5,0.3,1,0.3,0.5,0.9,0.7,0.2,0.5,0.625,0.417,0.417,0.625,2.083,0.00754717\n');  
+  end
+    fprintf(fid,'end bc\n');
 
 fprintf(fid,'\n');
 fprintf(fid,'\n');
